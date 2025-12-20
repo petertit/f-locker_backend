99a54ec9c0a72cb86797af1c4e680ddd63cb21fe
titcoder <www.monpetertit@gmail.com>
2025-12-20 14:08:53 +0700

    fix:preflight_OPTIONS

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
