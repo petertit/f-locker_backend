5ecb0af5dec760b53d3ec04b9363bda6a1ccccbb
titcoder <www.monpetertit@gmail.com>
2025-12-21 21:01:27 +0700

    fix:raspi/status_not_found

--- src/routes/raspi.js
+++ src/routes/raspi.js
@@ -1,26 +1,16 @@
+// src/routes/raspi.js
 import express from "express";
 import RaspiController from "../app/controllers/RaspiController.js";
 
 const router = express.Router();
 
-router.get("/raspi/status", (req, res) => RaspiController.status(req, res));
-router.post("/raspi/capture", (req, res) => RaspiController.capture(req, res));
+// GET /raspi/status
+router.get("/status", (req, res) => RaspiController.status(req, res));
 
-router.post("/raspi/capture-batch", (req, res) =>
-  RaspiController.captureBatch(req, res)
-);
-router.post("/raspi/capture-remote-batch", (req, res) =>
-  RaspiController.captureRemoteBatch(req, res)
-);
+// POST /raspi/unlock
+router.post("/unlock", (req, res) => RaspiController.unlock(req, res));
 
-router.get("/raspi/recognize", (req, res) =>
-  RaspiController.recognize(req, res)
-);
-router.post("/raspi/recognize-remote", (req, res) =>
-  RaspiController.recognizeRemote(req, res)
-);
-
-router.post("/raspi/unlock", (req, res) => RaspiController.unlock(req, res));
-router.post("/raspi/lock", (req, res) => RaspiController.lock(req, res));
+// POST /raspi/lock
+router.post("/lock", (req, res) => RaspiController.lock(req, res));
 
 export default router;
