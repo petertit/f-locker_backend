ed7712fdb3c5671d1338115eb2989e5e8cb05fde
titcoder <www.monpetertit@gmail.com>
2025-12-29 11:39:20 +0700

    testdb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,11 +1,17 @@
+// src/config/db/index.js
 import mongoose from "mongoose";
 
 export default async function connectDB() {
   const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error("Missing MONGO_URI in .env");
+  if (!uri) throw new Error("Missing MONGO_URI");
+
+  const DB_NAME = process.env.DB_NAME || "boxdata";
 
   mongoose.set("strictQuery", true);
 
-  await mongoose.connect(uri);
-  console.log("✅ Connected to MongoDB Atlas");
+  await mongoose.connect(uri, {
+    dbName: DB_NAME,
+  });
+
+  console.log(`✅ MongoDB connected. db=${DB_NAME}`);
 }
