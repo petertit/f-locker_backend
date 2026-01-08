f32790a9ee2309045c1dc71dac76550c5292defb
titcoder <www.monpetertit@gmail.com>
2026-01-08 12:08:25 +0700

    clear

--- src/app/middlewares/sortMiddleware.js
+++ src/app/middlewares/sortMiddleware.js
@@ -1,5 +1,4 @@
 // server/src/app/middlewares/sortMiddleware.js
 export default function sortMiddleware(req, res, next) {
-  // dành cho trang handlebar list/sort (nếu bạn dùng)
   return next();
 }
